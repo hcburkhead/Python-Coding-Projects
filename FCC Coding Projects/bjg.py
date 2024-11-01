3e0e3c30a41075d6413e76d8d8bc46081a2731fa
Clay9446 <157165409+hcburkhead@users.noreply.github.com>
2024-11-01 10:08:15 -0400

    Created project files

---
+++ FCC Coding Projects/bjg.py
@@ -0,0 +1,10 @@
+#Blackjack.py
+
+suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
+suit = suits[2]
+rank = "K"
+value = 10
+
+print("Your card is: " + rank + " of "+ suit)
+for suit in suits:
+    print(suit)
